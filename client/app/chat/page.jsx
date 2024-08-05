77fe1785d6206dffda8c9ff1bec47fd2954f3b5c
Cryptid <09akshatmishra@gmail.com>
2024-08-05 23:19:02 +0530

    Initial 1

---
+++ client/app/chat/page.jsx
@@ -0,0 +1,8 @@
+const chat = () => {
+  return (
+    <div>
+      <h1>Chat</h1>
+    </div>
+  );
+};
+export default chat;
---
+++ client/app/community/page.jsx
@@ -0,0 +1,5 @@
+const comm = () => {
+  return <div>page</div>;
+};
+
+export default comm;
