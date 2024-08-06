85e2143d46c7ec6ebd0a385b9291e55889f18d8a
Cryptid <09akshatmishra@gmail.com>
2024-08-06 23:19:17 +0530

    Initial 3

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import Link from "next/link";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
-
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -12,38 +11,31 @@ export default function Navbar() {
   };
 
   return (
-    <nav className=" p-4">
-      <div className=" mx-auto flex justify-between items-center">
-        <Link href="/">
-          <div className="text-white text-lg font-bold">My Website</div>
-        </Link>
-        <div className="grid container">
-          <div className="md:hidden">
-            <button
-              onClick={toggleMenu}
-              className="text-white focus:outline-none"
-            >
-              {isOpen ? <CloseIcon /> : <MenuIcon />}
-            </button>
-          </div>
-          <div
-            className={`flex-col md:flex-row md:flex md:items-center w-full md:w-auto ${isOpen ? "block" : "hidden"} md:block`}
-          >
-            <Link href="/">
-              <div className="text-white px-3 py-2 block md:inline">Home</div>
-            </Link>
-            <Link href="/notes">
-              <div className="text-white px-3 py-2 block md:inline">Notes</div>
-            </Link>
-            <Link href="/community">
-              <div className="text-white px-3 py-2 block md:inline">
-                Community
-              </div>
-            </Link>
-            <Link href="/chat">
-              <div className="text-white px-3 py-2 block md:inline">Chat</div>
-            </Link>
-          </div>
+    <nav className="flex justify-between p-2 fixed top-0 w-full z-50">
+      <Link href="/">
+        <div>Nexus</div>
+      </Link>
+
+      <div>
+        <button
+          onClick={toggleMenu}
+          className="hidden md:block text-white focus:outline-none"
+        >
+          {isOpen ? <CloseIcon /> : <MenuIcon />}
+        </button>
+        <div className={`${isOpen ? `block` : `hidden`} gap-3 grid md:flex`}>
+          <Link href="/">
+            <div>Home</div>
+          </Link>
+          <Link href="/notes">
+            <div>Notes</div>
+          </Link>
+          <Link href="/community">
+            <div>Community</div>
+          </Link>
+          <Link href="/chat">
+            <div>Chat</div>
+          </Link>
         </div>
       </div>
     </nav>
